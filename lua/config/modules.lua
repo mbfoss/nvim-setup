ddeb21a0365b94b82e1a54db9b7323d2324e4c88
mbfoss <mbfoss@fastmail.com>
2026-05-05 09:06:59 +0200

    Update

--- lua/config/modules.lua
+++ lua/config/modules.lua
@@ -20,6 +20,7 @@ local modules = {
 	{ pack = "keystone.nvim",     module = "keystone.explore",  with_config = false },
 	{ pack = "keystone.nvim",     module = "keystone.lspwords", with_config = false },
 	{ pack = "keystone.nvim",     module = "keystone.animate",  with_config = false },
+	{ pack = "keystone.nvim",     module = "keystone.objects",  with_config = false },
 	{ pack = "flash.nvim",        module = "flash",             with_config = true },
 	{ pack = "nvim-web-devicons", module = "nvim-web-devicons", with_config = false },
 
