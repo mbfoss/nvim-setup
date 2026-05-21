10f615ecaac7ed62f1ffa1b68af13570c7e349e1
mbfoss <mbfoss@fastmail.com>
2026-05-22 01:31:22 +0200

    Update

--- lua/config/modules.lua
+++ lua/config/modules.lua
@@ -19,6 +19,7 @@ local modules = {
 	{ pack = "keystone.nvim",   module = "keystone.animate",  with_config = false },
 	{ pack = "keystone.nvim",   module = "keystone.objects",  with_config = false },
 	{ pack = "keystone.nvim",   module = "keystone.base16",   with_config = true },
+	{ pack = "keystone.nvim",   module = "keystone.focus",    with_config = false },
 	{ pack = "easytasks.nvim",  module = "easytasks",         with_config = false },
 	{ pack = "flash.nvim",      module = "flash",             with_config = true },
 
