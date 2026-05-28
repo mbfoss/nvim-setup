ff17246d7f85b6984bc211ef56d316a235dd6ad5
mbfoss <mbfoss@fastmail.com>
2026-05-29 00:33:16 +0100

    Revert "Init"
    This reverts commit a0ca61c7dfc258a15063d52ffb494f5d4f4155d1.

--- pack/plugins/opt/easytasks-debug.nvim/plugin/easytasks-debug.lua
+++
@@ -1,8 +0,0 @@
-if vim.fn.has("nvim-0.10") ~= 1 then
-    error("easytasks-debug.nvim requires Neovim >= 0.10")
-end
-
-local ok, easytasks = pcall(require, "easytasks")
-if ok then
-    easytasks.register_task_type("debug", "easytasks-debug.types.debug")
-end
