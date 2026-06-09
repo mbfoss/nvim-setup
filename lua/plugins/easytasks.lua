d24ebe18ae6d1b44b58ab523727ecc34d9f2a8b1
mbfoss <mbfoss@fastmail.com>
2026-06-09 20:45:02 +0200

    Update

--- lua/plugins/easytasks.lua
+++ lua/plugins/easytasks.lua
@@ -5,13 +5,13 @@ require("easytasks").setup({
 	}
 })
 -- run / restart
-vim.keymap.set("n", "<leader>lr", ":Easytasks run<CR>", { desc = "Run task", silent = true })
-vim.keymap.set("n", "<leader>lR", ":Easytasks restart<CR>", { desc = "Repeat last task", silent = true })
+vim.keymap.set("n", "<leader>lr", ":Task run<CR>", { desc = "Run task", silent = true })
+vim.keymap.set("n", "<leader>lR", ":Task restart<CR>", { desc = "Repeat last task", silent = true })
 -- status panel
-vim.keymap.set("n", "<leader>lt", ":Easytasks toggle<CR>", { desc = "Toggle status panel", silent = true })
-vim.keymap.set("n", "<leader>lp", ":Easytasks jump<CR>", { desc = "Jump to task", silent = true })
+vim.keymap.set("n", "<leader>lt", ":Task toggle<CR>", { desc = "Toggle status panel", silent = true })
+vim.keymap.set("n", "<leader>lp", ":Task jump<CR>", { desc = "Jump to task", silent = true })
 -- task control
-vim.keymap.set("n", "<leader>lk", ":Easytasks stop<CR>", { desc = "Stop a task", silent = true })
-vim.keymap.set("n", "<leader>lK", ":Easytasks stop_all<CR>", { desc = "Stop all tasks", silent = true })
-vim.keymap.set("n", "<leader>lc", ":Easytasks clear<CR>", { desc = "Clear finished tasks", silent = true })
-vim.keymap.set("n", "<leader>ld", ":Easytasks dispose<CR>", { desc = "Dispose a task", silent = true })
+vim.keymap.set("n", "<leader>lk", ":Task stop<CR>", { desc = "Stop a task", silent = true })
+vim.keymap.set("n", "<leader>lK", ":Task stop_all<CR>", { desc = "Stop all tasks", silent = true })
+vim.keymap.set("n", "<leader>lc", ":Task clear<CR>", { desc = "Clear finished tasks", silent = true })
+vim.keymap.set("n", "<leader>ld", ":Task dispose<CR>", { desc = "Dispose a task", silent = true })
