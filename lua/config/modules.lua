0a2333a8e4b2bef6883fee72c873301c17ead079
mbfoss <mbfoss@fastmail.com>
2026-06-09 11:06:25 +0200

    Update modules.lua

--- lua/config/modules.lua
+++ lua/config/modules.lua
@@ -1,14 +1,22 @@
 local modules = {
+
 	-- { pack = "nvtoolkit.nvim",       module = "nvtoolkit" },
 	--  { pack = "lualine.nvim",    module = "lualine" },
+
 	{ pack = "osv.nvim",             module = "osv" },
 	{ pack = "mason.nvim",           module = "mason" },
 	{ pack = "gitsigns.nvim",        module = "gitsigns" },
+
 	-- { pack = "loop.nvim",            module = "loop" },
 	-- { pack = "loop-build.nvim",      module = "loop-build" },
 	-- { pack = "loop-cmake.nvim",      module = "loop-cmake" },
 	-- { pack = "loop-debug.nvim",      module = "loop-debug" },
 	-- { pack = "loop-marks.nvim",      module = "loop-marks" },
+
+	{ pack = "tomltools.nvim",       module = "tomltools" },
+	{ pack = "easytasks.nvim",       module = "easytasks" },
+	{ pack = "easytasks-debug.nvim", module = "easytasks-debug" },
+
 	{ pack = "keystone.nvim",        module = "keystone.pick" },
 	{ pack = "keystone.nvim",        module = "keystone.filetree" },
 	{ pack = "keystone.nvim",        module = "keystone.explore" },
@@ -22,11 +30,9 @@ local modules = {
 	{ pack = "keystone.nvim",        module = "keystone.statusline" },
 	{ pack = "keystone.nvim",        module = "keystone.bookmarks" },
 	{ pack = "flash.nvim",           module = "flash" },
-	{ pack = "tomltools.nvim",       module = "tomltools" },
-	{ pack = "easytasks.nvim",       module = "easytasks" },
-	{ pack = "easytasks-debug.nvim", module = "easytasks-debug" },
 	{ pack = "which-key.nvim",       module = "which-key" },
 }
+
 local pack_loaded = {}
 for _, entry in ipairs(modules) do
 	if pack_loaded[entry.pack] == nil then
@@ -34,13 +40,11 @@ for _, entry in ipairs(modules) do
 		vim.cmd('packadd! ' .. entry.pack)
 	end
 end
-
 for _, entry in ipairs(modules) do
-	local config_path = 'plugins.' .. entry.module
-	local ok, _ = pcall(require, config_path)
-	if not ok then
 		local mod = require(entry.module)
-		if type(mod.setup) == 'function' then
+		local has_config = pcall(require, "plugins." .. entry.module)
+		if not has_config then
+		if type(mod.setup) == "function" then
 			mod.setup({})
 		end
 	end
