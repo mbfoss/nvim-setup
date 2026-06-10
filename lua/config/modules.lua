eed764741f4ed79f0973b5823c2021dc39563819
mbfoss <mbfoss@fastmail.com>
2026-06-10 07:02:03 +0200

    Update

--- lua/config/modules.lua
+++ lua/config/modules.lua
@@ -2,6 +2,7 @@ local modules = {
 
 	-- { pack = "nvtoolkit.nvim",       module = "nvtoolkit" },
 	--  { pack = "lualine.nvim",    module = "lualine" },
+	-- { pack = "snacks.nvim",           module = "snacks" },
 
 	{ pack = "osv.nvim",             module = "osv" },
 	{ pack = "mason.nvim",           module = "mason" },
@@ -22,7 +23,7 @@ local modules = {
 	{ pack = "keystone.nvim",        module = "keystone.explore" },
 	{ pack = "keystone.nvim",        module = "keystone.lspwords" },
 	{ pack = "keystone.nvim",        module = "keystone.notify" },
-	{ pack = "keystone.nvim",        module = "keystone.animate" },
+	-- { pack = "keystone.nvim",        module = "keystone.animate" },
 	{ pack = "keystone.nvim",        module = "keystone.objects" },
 	{ pack = "keystone.nvim",        module = "keystone.colors" },
 	{ pack = "keystone.nvim",        module = "keystone.focus" },
--- lua/plugins/loop.lua
+++ lua/plugins/loop.lua
@@ -11,8 +11,22 @@ require("loop").setup({
 	use_fd_find = false,
 })
 
-loop.register_macro('add', function(_, arg1, arg2)
-			local n1, n2 = tonumber(arg1),tonumber(arg2)
-			if not n1 or not n2 then return nil, "add macro error, invalid argument" end
-			return tostring(n1 + n2)
-		end)
+vim.keymap.set("n", "<leader>ll", ":Loop<CR>", { desc = "Select command", silent = true })
+vim.keymap.set("n", "<leader>lf", ":Loop workspace find_files<CR>", { desc = "Find files", silent = true })
+vim.keymap.set("n", "<leader>lg", ":Loop workspace grep_files<CR>", { desc = "Grep files", silent = true })
+vim.keymap.set("n", "<leader>lt", ":Loop statuspanel<CR>", { desc = "Toggle status panel", silent = true })
+vim.keymap.set("n", "<leader>lp", ":Loop page switch<CR>", { desc = "Switch main page", silent = true })
+vim.keymap.set("n", "<leader>lP", ":Loop page open<CR>", { desc = "Open page", silent = true })
+vim.keymap.set("n", "<leader>lr", ":Loop task<CR>", { desc = "Run task", silent = true })
+vim.keymap.set("n", "<leader>lR", ":Loop task repeat<CR>", { desc = "Repeat last task", silent = true })
+vim.keymap.set("n", "<leader>lk", ":Loop task terminate<CR>", { desc = "Terminate a task", silent = true })
+vim.keymap.set("n", "<leader>lK", ":Loop task terminate_all<CR>", { desc = "Terminate all tasks", silent = true })
+vim.keymap.set("n", "<leader>lc", ":Loop statuspanel clean<CR>", { desc = "Cleanup status panel", silent = true })
+
+vim.keymap.set("n", "<leader>mm", ":Loop mark set<CR>", { desc = "Set bookmark", silent = true })
+vim.keymap.set("n", "<leader>mn", ":Loop mark name<CR>", { desc = "Set named bookmark", silent = true })
+vim.keymap.set("n", "<leader>md", ":Loop mark delete<CR>", { desc = "Delete bookmark", silent = true })
+vim.keymap.set("n", "<leader>ml", ":Loop mark list <CR>", { desc = "Bookmarks list", silent = true })
+vim.keymap.set("n", "<leader>nn", ":Loop note set<CR>", { desc = "Set note", silent = true })
+vim.keymap.set("n", "<leader>nd", ":Loop note delete<CR>", { desc = "Delete note", silent = true })
+vim.keymap.set("n", "<leader>nl", ":Loop note list <CR>", { desc = "Notes list", silent = true })
