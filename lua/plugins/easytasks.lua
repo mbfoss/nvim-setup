aaeb61ebc80f97af57f101d398717fefccc9a946
mbfoss <mbfoss@fastmail.com>
2026-06-24 14:02:03 +0200

    Update

--- lua/plugins/easytasks.lua
+++ lua/plugins/easytasks.lua
@@ -5,7 +5,9 @@ vim.keymap.set("n", "<leader>lr", ":Task run<CR>", { desc = "Run task", silent =
 vim.keymap.set("n", "<leader>lR", ":Task rerun<CR>", { desc = "Repeat last task", silent = true })
 -- status panel
 vim.keymap.set("n", "<leader>lt", ":Task panel<CR>", { desc = "Toggle status panel", silent = true })
-vim.keymap.set("n", "<leader>lp", ":Task panel pick<CR>", { desc = "Jump to task/tab", silent = true })
+vim.keymap.set("n", "<leader>lp", function()
+  vim.cmd(vim.v.count1 .. "Task panel jump")
+end, { desc = "Jump to task tab N (count)", silent = true })
 vim.keymap.set("n", "<leader>lc", ":Task panel clear<CR>", { desc = "Clear finished tasks", silent = true })
 vim.keymap.set("n", "<leader>ld", ":Task panel remove<CR>", { desc = "Remove one finishted task", silent = true })
 -- task control
