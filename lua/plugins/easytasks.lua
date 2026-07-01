4812375e17bbd95eab420fc867d82cd7069401af
mbfoss <mbfoss@fastmail.com>
2026-07-01 16:29:50 +0200

    Update

--- lua/plugins/easytasks.lua
+++ lua/plugins/easytasks.lua
@@ -6,10 +6,12 @@ vim.keymap.set("n", "<leader>lR", ":Task rerun<CR>", { desc = "Repeat last task"
 -- status panel
 vim.keymap.set("n", "<leader>lt", ":Task panel<CR>", { desc = "Toggle status panel", silent = true })
 vim.keymap.set("n", "<leader>lp", function()
-  vim.cmd(vim.v.count1 .. "Task panel jump")
+	vim.cmd(vim.v.count1 .. "Task panel jump")
 end, { desc = "Jump to task tab N (count)", silent = true })
 vim.keymap.set("n", "<leader>lc", ":Task panel clear<CR>", { desc = "Clear finished tasks", silent = true })
 vim.keymap.set("n", "<leader>ld", ":Task panel remove<CR>", { desc = "Remove one finishted task", silent = true })
 -- task control
 vim.keymap.set("n", "<leader>lk", ":Task stop<CR>", { desc = "Stop a task", silent = true })
 vim.keymap.set("n", "<leader>lK", ":Task stop_all<CR>", { desc = "Stop all tasks", silent = true })
+
+require("easytasks").register_expression("add", function(_, a, b) vim.notify()  return tonumber(a) + tonumber(b) end, {desc = "add 2 numbers"})
