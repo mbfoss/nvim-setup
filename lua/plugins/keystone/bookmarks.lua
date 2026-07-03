0e9148f11e16c266f5add069adf555237f583c9a
mbfoss <mbfoss@fastmail.com>
2026-07-03 10:45:44 +0200

    Update

--- lua/plugins/keystone/bookmarks.lua
+++ lua/plugins/keystone/bookmarks.lua
@@ -3,6 +3,7 @@ local bookmarks = require("keystone.bookmarks")
 bookmarks.setup()
 
 vim.keymap.set("n", "<leader>mm", "<cmd>Bookmark<cr>", { desc = "Set named bookmark" })
+vim.keymap.set("n", "<leader>mM", "<cmd>Bookmark setlabel<cr>", { desc = "Set named bookmark" })
 vim.keymap.set("n", "<leader>md", "<cmd>Bookmark delete<cr>", { desc = "Bookmark" })
 vim.keymap.set("n", "<leader>ml", "<cmd>Bookmark list<cr>", { desc = "Bookmark list" })
 vim.keymap.set("n", "<leader>fm", "<cmd>Bookmark pick<cr>", { desc = "Pick a bookmark" })
